a371878f7a7affa74706748bcf2d0bc1ee912a79
정유빈 <youbeen2798@naver.com>
2023-04-26 00:23:11 +0900

    feat HotplaceController 의존관계 설정 및 add 메소드 구현 완료

---
+++ src/main/java/com/ssafy/tourmates/hotplace/controller/HotplaceController.java
@@ -0,0 +1,39 @@
+package com.ssafy.tourmates.hotplace.controller;
+
+
+import com.ssafy.tourmates.hotplace.controller.dto.AddHotplaceRequest;
+import com.ssafy.tourmates.hotplace.model.service.HotplaceService;
+import com.ssafy.tourmates.hotplace.model.service.dto.AddHotplaceDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/hotplace")
+public class HotplaceController {
+
+  private HotplaceService hotplaceService;
+
+  public HotplaceController(HotplaceService hotplaceService) {
+    this.hotplaceService = hotplaceService;
+  }
+
+  @GetMapping("/add")
+  public String addHotpalce(@RequestBody AddHotplaceRequest request) {
+
+    AddHotplaceDto dto = AddHotplaceDto.builder()
+        .name(request.getName())
+        .desc(request.getDesc())
+        .visitedDate(request.getVisitedDate())
+        .uploadFileName(request.getUploadFileName())
+        .contentTypeId(request.getContentTypeId())
+        .contentTypeId(request.getContentTypeId())
+        .build();
+
+    hotplaceService.add(dto);
+    return null;
+  }
+}
