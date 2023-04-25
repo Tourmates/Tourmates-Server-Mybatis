d695176125ffa94281683b89115512beed178a03
chaos0103 <lyt1228@naver.com>
2023-04-25 23:34:29 +0900

    feat: Authority handler 구현

--- src/main/java/com/ssafy/tourmates/member/model/Authority.java
+++ src/main/java/com/ssafy/tourmates/member/model/Authority.java
@@ -1,5 +1,13 @@
 package com.ssafy.tourmates.member.model;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum Authority {
-    CLIENT, ADMIN
+    CLIENT("CLIENT", "회원"), ADMIN("ADMIN", "관리자");
+
+    private final String key;
+    private final String title;
 }
