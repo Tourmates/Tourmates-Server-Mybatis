58a1351d4b78e6ccc85b3eb3507af8aec821420b
정유빈 <youbeen2798@naver.com>
2023-04-26 00:25:53 +0900

    feat MemberNotFoundException 예외 구현 완료

---
+++ src/main/java/com/ssafy/tourmates/common/exception/MemberNotFoundException.java
@@ -0,0 +1,8 @@
+package com.ssafy.tourmates.common.exception;
+
+public class MemberNotFoundException extends IllegalArgumentException{
+
+    public MemberNotFoundException(){
+        super("회원이 존재하지 않습니다.");
+    }
+}
