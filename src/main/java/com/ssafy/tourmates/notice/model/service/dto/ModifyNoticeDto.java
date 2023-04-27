1f3cf4cb3b60d41872e346f07d78befb1f5d50cc
youbeen2798 <youbeen2798@naver.com>
2023-04-27 15:44:43 +0900

    feat: ModifyNoticeDto 클래스 구현 완료

---
+++ src/main/java/com/ssafy/tourmates/notice/model/service/dto/ModifyNoticeDto.java
@@ -0,0 +1,19 @@
+package com.ssafy.tourmates.notice.model.service.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+public class ModifyNoticeDto {
+
+    private String title;
+    private String content;
+    private boolean top;
+
+    @Builder
+    public ModifyNoticeDto(String title, String content, boolean top){
+        this.title = title;
+        this.content = content;
+        this.top = top;
+    }
+}
