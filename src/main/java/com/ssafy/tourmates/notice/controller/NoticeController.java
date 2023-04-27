7e54f928c81f1205e0131dc94c897523f23eef8f
youbeen2798 <youbeen2798@naver.com>
2023-04-27 15:45:38 +0900

    feat: NoticeController 클래스 권한 확인 구현 완료

--- src/main/java/com/ssafy/tourmates/notice/controller/NoticeController.java
+++ src/main/java/com/ssafy/tourmates/notice/controller/NoticeController.java
@@ -2,9 +2,11 @@ package com.ssafy.tourmates.notice.controller;
 
 import com.ssafy.tourmates.member.model.Member;
 import com.ssafy.tourmates.notice.controller.dto.AddNoticeRequest;
+import com.ssafy.tourmates.notice.controller.dto.ModifyNoticeRequest;
 import com.ssafy.tourmates.notice.model.Notice;
 import com.ssafy.tourmates.notice.model.service.NoticeService;
 import com.ssafy.tourmates.notice.model.service.dto.AddNoticeDto;
+import com.ssafy.tourmates.notice.model.service.dto.ModifyNoticeDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -30,11 +32,14 @@ public class NoticeController {
     }
 
     @PostMapping("/add")
-    public String addNotice(@RequestBody AddNoticeRequest addNoticeRequest, @SessionAttribute("loginMember") Member loginMember, Model model){
+    public String addNotice(@RequestBody AddNoticeRequest addNoticeRequest, @SessionAttribute(value = "loginMember", required = false) Member loginMember, Model model){
 
         if(loginMember == null){
             return "loginForm"; //TODO: 나중에 바꾸기
         }
+
+        noticeService.checkAuthority(loginMember);
+
         AddNoticeDto dto = AddNoticeDto.builder()
                 .title(addNoticeRequest.getTitle())
                 .content(addNoticeRequest.getContent())
@@ -47,5 +52,22 @@ public class NoticeController {
         model.addAttribute("dto", dto);
 
         return "redirect:/notice/" + noticeId + "/detail";
+    }
+
+    @GetMapping("/modify/{noticeId}")
+    public String modifyNotice(@PathVariable("noticeId") Long noticeId, @SessionAttribute(value = "loginMember", required = false) Member loginMember, Model model){
+
+        noticeService.checkAuthority(loginMember);
+        ModifyNoticeDto modifyNoticeDto= noticeService.getNotice(noticeId);
+        model.addAttribute("modifyNoticeDto", modifyNoticeDto);
+
+        return "modifyForm"; //TODO: 나중에 바꾸기
+
+    }
+
+    @PostMapping("/modify")
+    public String modifyNotice(@RequestBody ModifyNoticeRequest modifyNoticeRequest, @SessionAttribute("loginMember") Member loginMember){
+        return null;
+
     }
 }
