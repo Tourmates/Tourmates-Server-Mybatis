63fac618985349d2a193ec47588be2989826c519
Chaos0103 <lyt1228@naver.com>
2023-04-28 09:19:59 +0900

    build: 패키징 변경
    - war -> jar

--- src/main/java/com/ssafy/tourmates/common/exception/NoticeNotFoundException.java
+++ src/main/java/com/ssafy/tourmates/common/exception/NoticeNotFoundException.java
@@ -1,7 +1,5 @@
 package com.ssafy.tourmates.common.exception;
 
-import org.graalvm.compiler.core.common.type.ArithmeticOpTable;
-
 public class NoticeNotFoundException extends IllegalArgumentException{
 
     public NoticeNotFoundException(){
