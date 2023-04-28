495249f3f6dd85d0b41dde0432189621b0d71e5b
youbeen2798 <youbeen2798@naver.com>
2023-04-28 16:43:21 +0900

    feat: AttractionInfoRepository 조회, 저장 추가

---
+++ src/main/java/com/ssafy/tourmates/attractionInfo/repository/AttractionInfoRepository.java
@@ -0,0 +1,13 @@
+package com.ssafy.tourmates.attractionInfo.repository;
+
+import com.ssafy.tourmates.attractionInfo.AttractionInfo;
+import org.w3c.dom.Attr;
+
+import java.util.Optional;
+
+public interface AttractionInfoRepository {
+
+    AttractionInfo save(AttractionInfo attractionInfo);
+
+    Optional<AttractionInfo> findById(Long id);
+}
---
+++ src/main/java/com/ssafy/tourmates/attractionInfo/repository/AttractionInfoRepositoryImpl.java
@@ -0,0 +1,25 @@
+package com.ssafy.tourmates.attractionInfo.repository;
+
+import com.ssafy.tourmates.attractionInfo.AttractionInfo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class AttractionInfoRepositoryImpl implements AttractionInfoRepository {
+
+    private final AttractionInfoMapper attractionInfoMapper;
+
+
+    @Override
+    public AttractionInfo save(AttractionInfo attractionInfo) {
+        return attractionInfoMapper.save(attractionInfo);
+    }
+
+    @Override
+    public Optional<AttractionInfo> findById(Long id) {
+        return attractionInfoMapper.findById(id);
+    }
+}
