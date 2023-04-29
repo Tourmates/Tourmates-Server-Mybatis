c9665a77c915f5b02ff0479d3c2ecb2c5be897c5
정유빈 <youbeen2798@naver.com>
2023-04-29 16:25:24 +0900

    feat: MemberController 작성 완료

--- src/main/java/com/ssafy/tourmates/controller/MemberController.java
+++ src/main/java/com/ssafy/tourmates/controller/MemberController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Slf4j
 @RequestMapping("/member")
 public class MemberController {
-
   private final MemberService memberService;
 
 }
