7b5bde0d12fc9f31270b8ba42fdb4a483fdbc727
정유빈 <youbeen2798@naver.com>
2023-04-29 16:23:51 +0900

    feat: NoticeController 작성 완료

--- src/main/java/com/ssafy/tourmates/controller/NoticeController.java
+++ src/main/java/com/ssafy/tourmates/controller/NoticeController.java
@@ -21,7 +21,6 @@ import org.springframework.web.bind.annotation.SessionAttribute;
 @RequestMapping("/notice")
 public class NoticeController {
 
-
   private NoticeService noticeService;
 
   public NoticeController(NoticeService noticeService) {
