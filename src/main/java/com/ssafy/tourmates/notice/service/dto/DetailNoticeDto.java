c45e298be4b9ee82f74be97b905b7e21578347a8
youbeen2798 <youbeen2798@naver.com>
2023-05-01 15:38:41 +0900

    feat: DetailNoticeDto field name 변경

--- src/main/java/com/ssafy/tourmates/notice/service/dto/DetailNoticeDto.java
+++ src/main/java/com/ssafy/tourmates/notice/service/dto/DetailNoticeDto.java
@@ -9,7 +9,7 @@ import java.time.LocalDateTime;
 @Data
 public class DetailNoticeDto {
 
-    private Long id;
+    private Long noticeId;
     private String title;
     private String content;
     private boolean top;
@@ -19,8 +19,8 @@ public class DetailNoticeDto {
     private LocalDateTime lastModifiedDate;
 
     @Builder
-    public DetailNoticeDto(Long id, String title, String content, boolean top, Member createdBy, Member lastModifiedBy, LocalDateTime createdDate, LocalDateTime lastModifiedDate){
-        this.id = id;
+    public DetailNoticeDto(Long noticeId, String title, String content, boolean top, Member createdBy, Member lastModifiedBy, LocalDateTime createdDate, LocalDateTime lastModifiedDate){
+        this.noticeId = noticeId;
         this.title = title;
         this.content = content;
         this.top = top;
