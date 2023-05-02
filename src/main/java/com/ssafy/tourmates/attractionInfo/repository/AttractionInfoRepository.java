33852915149e393c25b4dacd1c49b358064ffd3c
youbeen2798 <youbeen2798@naver.com>
2023-05-02 13:49:28 +0900

    feat : AttractionInfoRepository 클래스 조회 기능 추가

--- src/main/java/com/ssafy/tourmates/attractionInfo/repository/AttractionInfoRepository.java
+++ src/main/java/com/ssafy/tourmates/attractionInfo/repository/AttractionInfoRepository.java
@@ -1,8 +1,10 @@
 package com.ssafy.tourmates.attractionInfo.repository;
 
 import com.ssafy.tourmates.attractionInfo.AttractionInfo;
+import com.ssafy.tourmates.controller.dto.request.AttractionInfoSearchRequest;
 import org.w3c.dom.Attr;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface AttractionInfoRepository {
@@ -15,5 +17,5 @@ public interface AttractionInfoRepository {
 
     void deleteById(int contentId);
 
-
+    List<AttractionInfo> findByConditions(AttractionInfoSearchRequest condition);
 }
