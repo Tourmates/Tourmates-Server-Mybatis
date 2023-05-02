3ea99c57c895f2aa9b60fff37a2b823bc7226be0
youbeen2798 <youbeen2798@naver.com>
2023-05-02 13:23:57 +0900

    feat : GugunRepository 클래스 추가

--- src/main/java/com/ssafy/tourmates/attractionInfo/repository/gugun/GugunRepository.java
+++ src/main/java/com/ssafy/tourmates/attractionInfo/repository/gugun/GugunRepository.java
@@ -1,4 +1,9 @@
 package com.ssafy.tourmates.attractionInfo.repository.gugun;
 
+import com.ssafy.tourmates.attractionInfo.service.gugun.Gugun;
+
+import java.util.List;
+
 public interface GugunRepository {
+    List<Gugun> findBySidoCode(int sidoCode);
 }
