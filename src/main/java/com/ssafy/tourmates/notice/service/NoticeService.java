8e7946fc5ee620ee8dce2afe0694af1512ac766b
youbeen2798 <youbeen2798@naver.com>
2023-05-02 09:37:41 +0900

    feat: NoticeService 목록 기능 추가

--- src/main/java/com/ssafy/tourmates/notice/service/NoticeService.java
+++ src/main/java/com/ssafy/tourmates/notice/service/NoticeService.java
@@ -5,6 +5,8 @@ import com.ssafy.tourmates.notice.service.dto.AddNoticeDto;
 import com.ssafy.tourmates.notice.service.dto.DetailNoticeDto;
 import com.ssafy.tourmates.notice.service.dto.ModifyNoticeDto;
 
+import java.util.List;
+
 public interface NoticeService {
 
     public Long add(AddNoticeDto dto, Member loginMember);
@@ -14,6 +16,8 @@ public interface NoticeService {
     ModifyNoticeDto getModifyNotice(Long noticeId);
 
     DetailNoticeDto getDetailNotice(Long noticeId);
+
+    List<DetailNoticeDto> getAllNotice();
 
     Long edit(Long modifyId, ModifyNoticeDto dto);
 
