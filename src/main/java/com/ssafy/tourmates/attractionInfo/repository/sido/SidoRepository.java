2dc62736a245fab6282386c2f92b0630959cdf77
youbeen2798 <youbeen2798@naver.com>
2023-05-02 11:12:37 +0900

    feat : SidoRepository 조회 기능 추가

---
+++ src/main/java/com/ssafy/tourmates/attractionInfo/repository/sido/SidoRepository.java
@@ -0,0 +1,10 @@
+package com.ssafy.tourmates.attractionInfo.repository.sido;
+
+import com.ssafy.tourmates.attractionInfo.service.sido.Sido;
+
+import java.util.List;
+
+public interface SidoRepository {
+
+    List<Sido> findAll();
+}
---
+++ src/main/java/com/ssafy/tourmates/attractionInfo/repository/sido/SidoRepositoryImpl.java
@@ -0,0 +1,16 @@
+package com.ssafy.tourmates.attractionInfo.repository.sido;
+
+import com.ssafy.tourmates.attractionInfo.service.sido.Sido;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class SidoRepositoryImpl implements SidoRepository {
+
+
+    @Override
+    public List<Sido> findAll() {
+        return null;
+    }
+}
