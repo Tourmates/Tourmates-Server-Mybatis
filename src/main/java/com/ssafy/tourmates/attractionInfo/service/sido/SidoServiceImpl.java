00277103b4e59ef8417a956f522749c53b5fbdee
youbeen2798 <youbeen2798@naver.com>
2023-05-02 17:54:28 +0900

    fix : SidoServiceImpl log 추가

--- src/main/java/com/ssafy/tourmates/attractionInfo/service/sido/SidoServiceImpl.java
+++ src/main/java/com/ssafy/tourmates/attractionInfo/service/sido/SidoServiceImpl.java
@@ -3,21 +3,24 @@ package com.ssafy.tourmates.attractionInfo.service.sido;
 import com.ssafy.tourmates.attractionInfo.repository.sido.SidoRepository;
 import com.ssafy.tourmates.attractionInfo.service.dto.SidoDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class SidoServiceImpl implements SidoService{
 
     private final SidoRepository sidoRepository;
 
-
     @Override
     public List<SidoDto> findAll() {
         List<Sido> sidoList = sidoRepository.findAll();
+
+        log.info("sidoList: {}", sidoList.size());
 
         return sidoList.stream()
                 .map(sido -> new SidoDto(sido.getCode(), sido.getName()))
