30d002bef47f011ea065c0b1fe0cd2adda2e96e3
Chaos0103 <lyt1228@naver.com>
2023-05-02 11:17:10 +0900

    feat: 뷰 페이지 추가

--- src/main/java/com/ssafy/tourmates/controller/AttractionInfoController.java
+++ src/main/java/com/ssafy/tourmates/controller/AttractionInfoController.java
@@ -8,8 +8,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 import java.util.HashMap;
 import java.util.List;
 
