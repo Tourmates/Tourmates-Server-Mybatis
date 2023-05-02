924722956abfc2e1f5ba0833166520ca93625911
youbeen2798 <youbeen2798@naver.com>
2023-05-02 16:30:42 +0900

    fix: AttractionInfoNotFoundException 기본생성자 변경 완료

--- src/main/java/com/ssafy/tourmates/common/exception/AttractionInfoNotFoundException.java
+++ src/main/java/com/ssafy/tourmates/common/exception/AttractionInfoNotFoundException.java
@@ -1,11 +1,8 @@
 package com.ssafy.tourmates.common.exception;
 
-import lombok.NoArgsConstructor;
-
-@NoArgsConstructor
 public class AttractionInfoNotFoundException extends RuntimeException {
 
-    public AttractionInfoNotFoundException(String message) {
+    public AttractionInfoNotFoundException() {
         super("관광지 정보가 존재하지 않습니다.");
     }
 }
