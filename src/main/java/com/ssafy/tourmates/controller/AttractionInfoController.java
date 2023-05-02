0f12f3dda02b2e73f773c7e1933e0a517b3ba5d7
youbeen2798 <youbeen2798@naver.com>
2023-05-02 11:10:07 +0900

    feat : AttracionInfoController 조회 기능 추가

---
+++ src/main/java/com/ssafy/tourmates/controller/AttractionInfoController.java
@@ -0,0 +1,36 @@
+package com.ssafy.tourmates.controller;
+
+import com.ssafy.tourmates.attractionInfo.service.AttractionInfoService;
+import com.ssafy.tourmates.attractionInfo.service.dto.SidoDto;
+import com.ssafy.tourmates.attractionInfo.service.sido.SidoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.HashMap;
+import java.util.List;
+
+@Controller("/attraction")
+@RequiredArgsConstructor
+public class AttractionInfoController {
+
+
+    private final AttractionInfoService attractionInfoService;
+    private final SidoService sidoService;
+
+
+    @GetMapping("/search")
+    public String search(@RequestParam HashMap<String, Integer> paramMap){
+        int sidoCode = paramMap.get("sidoCode");
+        int gugunCode = paramMap.get("gugunCode");
+        int contentTypeId = paramMap.get("contentTypeId");
+
+        List<SidoDto> sidoList = sidoService.findAll();
+
+        return null;
+    }
+
+}
