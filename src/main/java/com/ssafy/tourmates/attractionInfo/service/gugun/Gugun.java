1ddd9d83bcfe050fd20eca35655afe630650e513
youbeen2798 <youbeen2798@naver.com>
2023-05-02 15:58:39 +0900

    fix: Gugun 클래스 기본 생성자 추가

--- src/main/java/com/ssafy/tourmates/attractionInfo/service/gugun/Gugun.java
+++ src/main/java/com/ssafy/tourmates/attractionInfo/service/gugun/Gugun.java
@@ -3,8 +3,10 @@ package com.ssafy.tourmates.attractionInfo.service.gugun;
 import com.ssafy.tourmates.attractionInfo.service.sido.Sido;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class Gugun {
 
     private Integer code;
