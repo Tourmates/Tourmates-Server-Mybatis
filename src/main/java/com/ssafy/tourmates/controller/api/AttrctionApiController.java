2b5d85d5ab9898cf4780f3a161a438281ad504f1
youbeen2798 <youbeen2798@naver.com>
2023-05-02 16:55:26 +0900

    fix : AttractionApiController search 메소드 추가 변경 완료

--- src/main/java/com/ssafy/tourmates/controller/api/AttrctionApiController.java
+++ src/main/java/com/ssafy/tourmates/controller/api/AttrctionApiController.java
@@ -2,30 +2,60 @@ package com.ssafy.tourmates.controller.api;
 
 import com.ssafy.tourmates.attractionInfo.service.AttractionInfoService;
 import com.ssafy.tourmates.attractionInfo.service.dto.AttractionInfoDto;
+import com.ssafy.tourmates.controller.dto.request.AttractionInfoSearchRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
+@Slf4j
 @RequestMapping("/api/attraction")
 public class AttrctionApiController {
 
     private final AttractionInfoService attractionInfoService;
 
     @GetMapping("/search")
-    public String searchAttractionInfo(@RequestParam HashMap<String, Integer> paramMap){
+    public void searchAttractionInfo(HttpServletResponse response,
+                                       @RequestParam HashMap<String, Integer> paramMap) throws IOException {
 
-        int contentID = paramMap.get("contentId");
-        AttractionInfoDto dto = attractionInfoService.searchAttraction(contentID);
+        int sidoCode = paramMap.get("sidoCode");
+        int gugunCode = paramMap.get("gugunCode");
+        int contentTypeId = paramMap.get("contentTypeId");
 
-      ///  JSONObject json = new JSONObject();
-        return null;
+        AttractionInfoSearchRequest condition = AttractionInfoSearchRequest.builder()
+                        .sidoCode(sidoCode)
+                        .gugunCode(gugunCode)
+                        .contentTypeId(contentTypeId)
+                        .build();
+
+        List<AttractionInfoDto> attractionList = attractionInfoService.searchAttraction(condition);
+
+        JSONObject json = new JSONObject();
+        JSONArray array = new JSONArray();
+
+        for(AttractionInfoDto attraction : attractionList){
+            JSONObject temp = new JSONObject();
+            temp.put("title", attraction.getTitle());
+            temp.put("addr1", attraction.getAddr1());
+            temp.put("zipcode", attraction.getZipcode());
+            temp.put("firstImage", attraction.getFirstImage());
+            temp.put("latitude", attraction.getLatitude());
+            temp.put("longtitude", attraction.getLongitude());
+            array.add(temp);
+        }
+
+        response.setContentType("application/json; charset=UTF-8");
+        response.getWriter().println(json);
 
     }
 
