1c1fdeaecd243d8bb77bb799a33b8adeb0d16d32
youbeen2798 <youbeen2798@naver.com>
2023-05-02 13:48:15 +0900

    feat : AttractionInfoController 조회 기능 추가

--- src/main/java/com/ssafy/tourmates/controller/AttractionInfoController.java
+++ src/main/java/com/ssafy/tourmates/controller/AttractionInfoController.java
@@ -1,10 +1,15 @@
 package com.ssafy.tourmates.controller;
 
 import com.ssafy.tourmates.attractionInfo.service.AttractionInfoService;
+import com.ssafy.tourmates.attractionInfo.service.dto.AttractionInfoDto;
+import com.ssafy.tourmates.attractionInfo.service.dto.GugunDto;
 import com.ssafy.tourmates.attractionInfo.service.dto.SidoDto;
+import com.ssafy.tourmates.attractionInfo.service.gugun.GugunService;
 import com.ssafy.tourmates.attractionInfo.service.sido.SidoService;
+import com.ssafy.tourmates.controller.dto.request.AttractionInfoSearchRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -18,17 +23,34 @@ public class AttractionInfoController {
 
     private final AttractionInfoService attractionInfoService;
     private final SidoService sidoService;
+    private final GugunService gugunService;
 
 
     @GetMapping("/search")
-    public String search(@RequestParam HashMap<String, Integer> paramMap){
+    public String search(@RequestParam HashMap<String, Integer> paramMap, Model model){
         int sidoCode = paramMap.get("sidoCode");
         int gugunCode = paramMap.get("gugunCode");
         int contentTypeId = paramMap.get("contentTypeId");
 
         List<SidoDto> sidoList = sidoService.findAll();
 
-        return null;
+        model.addAttribute("sidos", sidoList);
+
+        List<GugunDto> gugunList = gugunService.searchGuguns(sidoCode);
+
+        model.addAttribute("guguns", gugunList);
+
+        AttractionInfoSearchRequest condition = AttractionInfoSearchRequest.builder()
+                .sidoCode(sidoCode)
+                .gugunCode(gugunCode)
+                .contentTypeId(contentTypeId)
+                .build();
+
+        List<AttractionInfoDto> attractionInfoDtoList = attractionInfoService.searchAttraction(condition);
+
+        model.addAttribute("attractions", attractionInfoDtoList);
+
+        return "attraction/attractionList";
     }
 
 }
