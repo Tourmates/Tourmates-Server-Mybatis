6571a36785d45822be2fd937f7933cc6325dca66
youbeen2798 <youbeen2798@naver.com>
2023-05-02 13:20:26 +0900

    feat : Gugun 클래스 추가

---
+++ src/main/java/com/ssafy/tourmates/attractionInfo/service/gugun/Gugun.java
@@ -0,0 +1,20 @@
+package com.ssafy.tourmates.attractionInfo.service.gugun;
+
+import com.ssafy.tourmates.attractionInfo.service.sido.Sido;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class Gugun {
+
+    private Integer code;
+    private String name;
+    private Sido sido;
+
+    @Builder
+    public Gugun(Integer code, String name, Sido sido) {
+        this.code = code;
+        this.name = name;
+        this.sido = sido;
+    }
+}
